02b92145168f374fbcabb6f3df49371f1ce64c9c
woogisea <rgn95652@gmail.com>
2023-07-27 10:51:42 +0900

    feat : telegram_bot

---
+++ server.js
@@ -0,0 +1,66 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const TelegramBot = require("node-telegram-bot-api");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// replace the value below with the Telegram token you receive from @BotFather
+
+const token = process.env.TELEGRAM_BOT_TOKEN;
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, { polling: true });
+
+let chatId = 500;
+
+bot.on("message", (msg) => {
+  chatId = msg.chat.id;
+});
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+
+  // Replace 'path/to/your/image.jpg' with the actual path to your image file
+  const message = "Let's start!\nPlease connect wallet first";
+  const button1 = {
+    text: "Open Nexton",
+    web_app: { url: "https://nexton-front.vercel.app/" },
+  };
+
+  // Create an array of inline button rows (each row is an array of button options)
+  const inlineButtons = [[button1]];
+
+  const buttonOptions = {
+    reply_markup: {
+      inline_keyboard: inlineButtons,
+    },
+  };
+
+  bot
+    .sendMessage(chatId, message, {
+      parse_mode: "HTML",
+      ...buttonOptions,
+    })
+    .catch((error) => {
+      console.error("Error sending photo:", error);
+    });
+});
+
+bot.onText(/\/stake (\d+) (\d+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const var1 = match[1]; // the first variable
+  const var2 = match[2]; // the second variable
+
+  bot.sendMessage(chatId, `You staked ${var1} and ${var2}.`);
+});
+
+app.get("/chatId", (req, res) => {
+  res.send({ chatId });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
